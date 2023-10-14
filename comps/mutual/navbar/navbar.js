199683db7186be776c870a10a408506cfc104f19
bb <bohdan.buchok@ukma.edu.ua>
2023-10-14 21:43:20 +0300

    link mixin added, navbar styled

--- comps/mutual/navbar/navbar.js
+++ comps/mutual/navbar/navbar.js
@@ -1,8 +1,27 @@
+import Link from "next/link";
 import s from "./navbar.module.scss";
+
 const Navbar = () => {
   return (
     <>
-      <div className={`${s.navbar}`}></div>
+      <div className={`${s.navbar}`}>
+        <div
+          role="button"
+          tabIndex="0"
+          onClick={() => router.push(`/`)}
+          className={`${s.logo}`}
+        >
+          <img src="/general/favicon.ico" alt="logo" />
+        </div>
+        <div className={`${s.links_container}`}>
+          <Link href="/" className={`${s.link}`}>
+            Heroes
+          </Link>
+          <Link href="/about" className={`${s.link}`}>
+            About
+          </Link>
+        </div>
+      </div>
     </>
   );
 };
