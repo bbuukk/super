da983702eaf2a7d3aa8c74c3a0d8e8706deb2fba
bb <bohdan.buchok@ukma.edu.ua>
2023-10-14 21:11:44 +0300

    [heroId.js page init setup]

---
+++ pages/[heroId].js
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import { useHeroContext } from "@/back/hooks/useHeroContext";
+
+const Hero = () => {
+  const router = useRouter();
+  const { heroId } = router.query;
+  const { heroes } = useHeroContext();
+
+  const [hero, setHero] = useState({});
+
+  useEffect(() => {
+    if (heroes) {
+      setHero(heroes[heroId]);
+    }
+  }, [heroes, heroId]);
+
+  return (
+    <div>
+      <h1>{hero.nickname}</h1>
+      <p>Real Name: {hero.real_name}</p>
+      <p>Origin Description: {hero.origin_description}</p>
+      <ul>
+        {hero.superpowers &&
+          hero.superpowers.map((power, index) => <li key={index}>{power}</li>)}
+      </ul>
+      <p>Catch Phrase: {hero.catch_phrase}</p>
+      <div>
+        {hero.images &&
+          hero.images.map((image, index) => (
+            <img key={index} src={image} alt={`${hero.nickname} image`} />
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
