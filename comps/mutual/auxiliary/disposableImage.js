5c579f61ea5f30d6850b3357adf9977ca3dabedf
bb <bohdan.buchok@ukma.edu.ua>
2023-10-15 11:35:20 +0300

    disposable image update

--- comps/mutual/auxiliary/disposableImage.js
+++ comps/mutual/auxiliary/disposableImage.js
@@ -1,13 +1,14 @@
+import Image from "next/image";
 import s from "./disposableImage.module.scss";
 
-const DisposableImage = () => {
+const DisposableImage = ({ imageUrl, dispose }) => {
   return (
     <div className={`${s.disposable_image}`}>
-      <button onCLick={() => {}} className={`${s.dispose_btn}`}>
+      <button onCLick={dispose} className={`${s.dispose_btn}`}>
         <i class="bi bi-x"></i>
       </button>
       <div className={`${s.image_container}`}>
-        <img src="/general/favicon.ico" alt="logo" />
+        <Image src={imageUrl} alt="hero image" />
       </div>
     </div>
   );
